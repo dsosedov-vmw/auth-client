c0fcbaeccd101da877203f6e9922ea9988a1ac32
Dmitrii Sosedov <dsosedov@vmware.com>
2021-10-21 12:41:08 -0400

    Init

---
+++ src/test/java/com/dsosedov/authclient/AuthClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsosedov.authclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
